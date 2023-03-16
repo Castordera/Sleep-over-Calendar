6d102f7174496670e934640984026f47c049ea32
Ulises Castorena <u.castorenacaldera@olx.pl>
2023-03-16 15:37:10 +0100

    Add Timber and Firebase auth

--- app/src/main/java/com/example/sleepschedule/ScheduleApp.kt
+++ app/src/main/java/com/example/sleepschedule/ScheduleApp.kt
@@ -2,7 +2,15 @@ package com.example.sleepschedule
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ScheduleApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
