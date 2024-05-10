6538710d9b7cdbbe91da7065eff11030d14aac27
Ulises Castorena <u.castorenacaldera@olx.pl>
2024-05-10 15:58:22 +0200

    Enable Edge to Edge

--- design/theme/src/main/kotlin/com/ulises/theme/Theme.kt
+++ design/theme/src/main/kotlin/com/ulises/theme/Theme.kt
@@ -56,8 +56,8 @@ fun SleepScheduleTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
