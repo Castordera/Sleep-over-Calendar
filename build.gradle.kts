e2ea450aed1a6fd4b6a02ea216470e6a9835c1a7
Ulises Castorena <u.castorenacaldera@olx.pl>
2024-05-10 15:58:12 +0200

    Update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
-    id("com.google.gms.google-services") version("4.3.15") apply false
+    alias(libs.plugins.google.services) apply false
 }
